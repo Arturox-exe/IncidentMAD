ad2b72f5286212d06e8eb5ed76daf1d1347e5321
alba-rico <alba.rico.rodriguez@alumnos.upm.es>
2021-11-06 14:21:43 +0100

    Parseo del archivo XML

--- app/src/main/java/dte/masteriot/mdp/finalproyect_mobiledevices/IncidentsActivity.java
+++ app/src/main/java/dte/masteriot/mdp/finalproyect_mobiledevices/IncidentsActivity.java
@@ -1,26 +1,120 @@
 package dte.masteriot.mdp.finalproyect_mobiledevices;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class IncidentsActivity extends AppCompatActivity {
 
-    Intent inputIntent;
-    String text;
-    TextView tvContent;
+    private static final String URL_INCIDENTS = "https://informo.madrid.es/informo/tmadrid/incid_aytomadrid.xml";
+    private static final List<Item> listOfItems = new ArrayList<>();
+    private static final String UNKNOWN_NAME = "Unknown name";
+
+    private RecyclerView myRecycleView;
+
+    HttpURLConnection urlConnection;
+    ExecutorService es;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incidents);
 
-        tvContent= findViewById(R.id.textView);
-        inputIntent = getIntent();
+        myRecycleView = findViewById(R.id.recyclerView);
+        myRecycleView.setLayoutManager(new LinearLayoutManager(this));
 
-        text=inputIntent.getStringExtra("xmlURL");
-
-        tvContent.setText(text);
+        es = Executors.newSingleThreadExecutor();
+        LoadURLContents loadURLContents = new LoadURLContents(handler, URL_INCIDENTS);
+        es.execute(loadURLContents);
     }
+
+    // Define the handler that will receive the messages from the background thread:
+    Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            parseXml();
+        }
+    };
+
+    private void parseXml() {
+        try {
+
+            XmlPullParser parser = XmlPullParserFactory.newInstance().newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(LoadURLContents.is, null);
+
+            String name = "";
+            String id = "";
+            String cod = "";
+            String inDate = "";
+            String fiDate = "";
+            String description = "";
+            String Long = "";
+            String lat = "";
+
+            int eventType = parser.getEventType();
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    String elementName = parser.getName();
+                    if (elementName.equals("nom_tipo_incidencia")) {
+                        name=parser.nextText();
+                    }
+                    else if(elementName.equals("id_incidencia")){
+                        id=parser.nextText();
+                    }
+                    else if(elementName.equals("tipoincid")){
+                        cod=parser.nextText();
+                    }
+                    else if(elementName.equals("fh_inicio")){
+                        inDate=parser.nextText();
+                    }
+                    else if(elementName.equals("fh_final")){
+                        fiDate=parser.nextText();
+                    }
+                    else if(elementName.equals("descripcion")){
+                        description=parser.nextText();
+                    }
+                    else if(elementName.equals("longitud")){
+                        Long=parser.nextText();
+                    }
+                    else if(elementName.equals("latitud")){
+                        lat=parser.nextText();
+                    }
+                }
+                listOfItems.add(new Item(name,id,cod,inDate,fiDate,description,Long,lat));
+                eventType = parser.next(); // Get next parsing event
+            }
+            urlConnection.disconnect();
+        } catch (Exception e) {
+            Toast.makeText(this, "Error:" + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
--- app/src/main/java/dte/masteriot/mdp/finalproyect_mobiledevices/MainActivity.java
+++ app/src/main/java/dte/masteriot/mdp/finalproyect_mobiledevices/MainActivity.java
@@ -1,25 +1,18 @@
 package dte.masteriot.mdp.finalproyect_mobiledevices;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String URL_INCIDENTS = "https://informo.madrid.es/informo/tmadrid/incid_aytomadrid.xml";
-
     Button btIncidents, btStatistics, btForum;
-    ExecutorService es;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,38 +23,10 @@ public class MainActivity extends AppCompatActivity {
         btStatistics= (Button) findViewById(R.id.statistics_button);
         btForum= (Button) findViewById(R.id.forum_button);
 
-        toggle_buttons(true);
-
-        es = Executors.newSingleThreadExecutor();
-    }
-
-    Handler handler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            String string_result;
-            super.handleMessage(msg);
-
-            if((string_result = msg.getData().getString("content")) != null) {
-               newActivity(string_result);
-            }
-        }
-    };
-
-    private void toggle_buttons(boolean state) {
-        btIncidents.setEnabled(state);
-        btStatistics.setEnabled(state);
-        btForum.setEnabled(state);
     }
 
     public void onIncidentsClick(View view) {
-        toggle_buttons(false);
-        LoadURLContents loadURLContents = new LoadURLContents(handler, URL_INCIDENTS);
-        es.execute(loadURLContents);
-    }
-
-    public void newActivity (String string_result){
         Intent i = new Intent(this,IncidentsActivity.class);
-        i.putExtra("xmlURL", string_result);
         startActivity(i);
     }
 
