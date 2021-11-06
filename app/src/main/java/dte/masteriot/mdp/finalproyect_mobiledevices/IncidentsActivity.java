e90b30baeda157c38c1249e8b43af162d38e0f04
alba-rico <alba.rico.rodriguez@alumnos.upm.es>
2021-11-06 10:41:49 +0100

    Actividad Incidents

---
+++ app/src/main/java/dte/masteriot/mdp/finalproyect_mobiledevices/IncidentsActivity.java
@@ -0,0 +1,11 @@
+package dte.masteriot.mdp.finalproyect_mobiledevices;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IncidentsActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incidents);
+    }
+}
