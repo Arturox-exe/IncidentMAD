897ad9b90d7cc7b5002039699f4e7bed7550422d
alba-rico <alba.rico.rodriguez@alumnos.upm.es>
2021-11-07 01:34:12 +0100

    Revert "Merge branch 'map' into develop"
    This reverts commit b32ef00ba252224b854ece65b42db23bc74bbfe8, reversing
    changes made to 1c67f80f85ad1780145a8e1c2a313987483dda6e.

--- app/src/main/java/dte/masteriot/mdp/finalproyect_mobiledevices/Incident.java
+++ app/src/main/java/dte/masteriot/mdp/finalproyect_mobiledevices/Incident.java
@@ -1,18 +1,24 @@
 package dte.masteriot.mdp.finalproyect_mobiledevices;
 
-import com.google.android.gms.maps.model.LatLng;
-
 public class Incident {
     private final String name;
     private final String codInc;
     private final String description;
+<<<<<<< HEAD
     private final LatLng coordinates;
+=======
+   // private final LatLng coordenates;
+>>>>>>> parent of 579f7ac ([ADD] Maps activity)
 
     Incident(String name, String cod, String description, LatLng coordinates) {
         this.name = name;
         this.codInc=cod;
         this.description=description;
+<<<<<<< HEAD
         this.coordinates = coordinates;
+=======
+       // this.coordenates = coordenates;
+>>>>>>> parent of 579f7ac ([ADD] Maps activity)
     }
 
     String getName() {
@@ -27,9 +33,15 @@ public class Incident {
         return description;
     }
 
+<<<<<<< HEAD
     public LatLng getCoordinates() {
         return coordinates;
     }
+=======
+    /*public LatLng getCoordenates() {
+        return coordenates;
+    }*/
+>>>>>>> parent of 579f7ac ([ADD] Maps activity)
 
     boolean isLocationValid() {
         return (coordinates != null);
